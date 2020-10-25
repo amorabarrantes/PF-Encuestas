a0b9d009a7e3864abeec663c488121732dbdb8fb
andres <andrestati17@gmail.com>
2020-10-25 17:45:02 -0600

    Respuestas listas

--- hello.hs
+++ hello.hs
@@ -1,16 +1,7 @@
-
-listaEncuestas = ["Encuesta1", "Encuesta2"]
-
-
-
-listaRespuestas = [ [ ["E1pregunta1Respuesta1","E1pregunta1Respuesta2"],["E1pregunta2Respuesta1","E1pregunta2Respuesta2"] ] , [["hola"],["hola2"]] ]
-
 --Funcion para pedir inputs al usuario.
 input ::  IO String
 input  = do
     getLine
-
-
 
 --Funcion para agregar preguntas.
 --[["Encuesta1"],["Encuesta2"], ["etc"]]
@@ -20,8 +11,6 @@ agregarEncuestas listaVacia = do
     let lista = listaVacia ++ [[pregunta]]
 
     return(lista)
-
-
 
 --Funcion para agregar preguntas
 --[["Pregunta1", "Pregunta2"]]
@@ -49,8 +38,6 @@ agregarPreguntasAux listaP = do
             agregarPreguntasAux listaNueva2
        else return(listaP)
 
-
-
 --Funcion para agregar respuestas
 --[ [ ["Respuesta1","Respuesta2"], ["Respuesta1","Respuesta2"] ] ]
 --Dentro de los morados estan las respuestas para cada cuestionario.
@@ -69,26 +56,47 @@ agregarRespuestas listaVacia = do
     else return([listaVacia])
 
 
+
+
+
+responderEncuestas :: [[String]]->[[String]] ->IO [[String]]
+responderEncuestas listaPR listaRetorno  = do
+    
+    if (listaPR /= [])
+        then do
+            if (length(head listaPR) /= 1)
+                then do
+                    print("Digite el indice a responder para la pregunta")
+                    print(head listaPR)
+                    indice <- input
+                    let indiceInt = read indice :: Int  
+
+                    let listaResultados = head listaPR !! indiceInt
+                    let listaRetornoAux = (listaRetorno ++ [[listaResultados]])
+                    responderEncuestas (tail listaPR) listaRetornoAux
+                else do
+                    let listaResultados = head listaPR !! 0
+                    let listaRetornoAux = (listaRetorno ++ [[listaResultados]])
+                    responderEncuestas (tail listaPR) listaRetornoAux
+            
+       else return(listaRetorno)
+
+
+
 indicesOf :: Eq a => a -> [a] -> [Int]
 indicesOf a as = [i | (b, i) <- zip as [0..], b == a]
+
 
 listaprueba = ["hola", "adios", "xd"]
 listaPreguntas = [ [ ["pregunta1"],["respuesta1x", "respuestaN"], ["pregunta2"],["respuesta1", "respuesta2", "respuestaN"] ] ]
 
 main :: IO()
 main = do
-    --(mapM_) print (listaPreguntas !!0)
-    print(listaPreguntas!!0)
-    print(indicesOf "respuesta1x" ((listaPreguntas!!0)!!1))
+    respuestas <- responderEncuestas (listaPreguntas!!0) []
+    print(respuestas)
+    --print(listaPreguntas!!0)
+    --print(indicesOf "respuesta1x" ((listaPreguntas!!0)!!1))
 
-    --print(buscarIndice (listaPreguntas !! 0) 0)
-
-
-    --print((listaRespuestas !! 0))
-    --print(listaEncuestas !! 0)
-    --print(listaPreguntas !! 0)
-    --print(listaRespuestas !! 0)
-    
     --a <- agregarEncuestas []
     --b <- agregarPreguntas []
     --c <- agregarRespuestas []
